5878b0976853ad5abd14769e487e10f042a2a2d8
rxfe-a <28RafaelA539@gmail.com>
2025-12-10 11:56:53 -0800

    scale factor

--- source/main.py
+++ source/main.py
@@ -3,9 +3,11 @@ from PyQt6.QtGui import QPainter, QPixmap, QGuiApplication
 from PyQt6.QtCore import Qt, QTimer
 import sys
 
-class Overlay(QWidget): ##test widget obv gonna be rewritten for the crosshair
+class Overlay(QWidget):
     def __init__(self):
         super().__init__()
+
+        # window flags (i think i did it wrong tho)
         self.setWindowFlags(
             Qt.WindowType.FramelessWindowHint |
             Qt.WindowType.WindowStaysOnTopHint |
@@ -14,10 +16,20 @@ class Overlay(QWidget): ##test widget obv gonna be rewritten for the crosshair
         )
 
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
-        self.image = QPixmap("assets/menu_sprite.png")
+        original = QPixmap("assets/menu_sprite.png")
+        scale_factor = 1.3
+        self.image = original.scaled(
+            int(original.width() * scale_factor),
+            int(original.height() * scale_factor),
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation
+        )
+
         self.show_image = True
+
         screen = QGuiApplication.primaryScreen().geometry()
         self.setGeometry(screen)
+
         QTimer.singleShot(0, self.update_loop)
 
     def update_loop(self):
@@ -29,12 +41,10 @@ class Overlay(QWidget): ##test widget obv gonna be rewritten for the crosshair
             return
 
         painter = QPainter(self)
-
         img_w = self.image.width()
         img_h = self.image.height()
-
         x = self.width() - img_w - 20
-        y = 20                         
+        y = 20
 
         painter.drawPixmap(x, y, self.image)
 
@@ -42,6 +52,9 @@ class Overlay(QWidget): ##test widget obv gonna be rewritten for the crosshair
         if event.key() == Qt.Key.Key_F2:
             self.show_image = not self.show_image
             self.update()
+
+        if event.key() == Qt.Key.Key_F3:
+            QApplication.quit()
 
 
 if __name__ == "__main__":
